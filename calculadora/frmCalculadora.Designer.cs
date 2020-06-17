8f2c785b1447dcfa657bb7bde869db80b4c4f833
angelbaezsa <47637565+angelbaezsa@users.noreply.github.com>
2020-06-16 21:36:34 -0400

    Label, texto y boton calcular creado

---
+++ calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblnumero1 = new System.Windows.Forms.Label();
+            this.lblnumero2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblnumero1
+            // 
+            this.lblnumero1.AutoSize = true;
+            this.lblnumero1.Location = new System.Drawing.Point(19, 36);
+            this.lblnumero1.Name = "lblnumero1";
+            this.lblnumero1.Size = new System.Drawing.Size(51, 13);
+            this.lblnumero1.TabIndex = 1;
+            this.lblnumero1.Text = "numero 1";
+            this.lblnumero1.Click += new System.EventHandler(this.lblnumero1_Click);
+            // 
+            // lblnumero2
+            // 
+            this.lblnumero2.AutoSize = true;
+            this.lblnumero2.Location = new System.Drawing.Point(17, 83);
+            this.lblnumero2.Name = "lblnumero2";
+            this.lblnumero2.Size = new System.Drawing.Size(53, 13);
+            this.lblnumero2.TabIndex = 2;
+            this.lblnumero2.Text = "Numero 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 224);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblnumero2);
+            this.Controls.Add(this.lblnumero1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblnumero1;
+        private System.Windows.Forms.Label lblnumero2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
